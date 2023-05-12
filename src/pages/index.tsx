2ca68880621a17313d2d54cf31921db99bd25a04
Daniel Kunkler <kunkl007@umn.edu>
2023-05-12 18:02:25 -0500

    Made a day and week component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,17 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 import { SignInButton, useUser, UserButton } from "@clerk/nextjs";
 import Calendar from "react-calendar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { add } from "date-fns";
 
 const CalendarWizard = () => {
   const { user } = useUser();
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState<undefined | Date>();
+  useEffect(() => {
+    console.log(date);
+  }, [date]);
+  console.log(Date.now());
+  console.log();
   if (!user) return null;
   return (
     <div>
@@ -19,47 +24,89 @@ const CalendarWizard = () => {
           if (event) setDate(event as Date);
         }}
         value={date}
+        returnValue="range"
         className={"react-calendar"}
+        selectRange={true}
         // showWeekNumbers={true}
       />
+      {date && (
+        <table>
+          <tr>
+            <th>Week</th>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tues</th>
+            <th>Wed</th>
+            <th>Thurs</th>
+            <th>Fri</th>
+            <th>Sat</th>
+          </tr>
+        </table>
+      )}
     </div>
   );
 };
 
-const FullTrainingCalendar = () => {
+// const FullTrainingCalendar = () => {
+//   const { user } = useUser();
+//   const [date, setDate] = useState(new Date());
+//   if (!user) return null;
+//   return (
+//     <div>
+//       <Calendar
+//         onChange={(event) => {
+//           if (event) setDate(event as Date);
+//         }}
+//         value={date}
+//         className={"react-calendar"}
+//         showNeighboringMonth={false}
+//         // showWeekNumbers={true}
+//       />
+//       <Calendar
+//         value={add(new Date(), { weeks: 4 })}
+//         className={"react-calendar"}
+//         showNeighboringMonth={false}
+//       />
+//       <Calendar
+//         value={add(new Date(), { weeks: 8 })}
+//         className={"react-calendar"}
+//         showNeighboringMonth={false}
+//       />
+//       <Calendar
+//         value={add(new Date(), { weeks: 12 })}
+//         className={"react-calendar"}
+//         showNeighboringMonth={false}
+//       />
+//     </div>
+//   );
+// };
+type DayProps = {
+  day: Date;
+};
+const Day = (props: DayProps) => {
+  const { day } = props;
+  return <div className="day">{day.toString()}</div>;
+};
+
+type WeekProps = {
+  startDate: Date;
+};
+const Week = (props: WeekProps) => {
+  const { startDate } = props;
+  const days = [];
+  for (let i = 0; i < 7; i++) {
+    days.push(<Day day={add(startDate, { days: i })} />);
+  }
+};
+
+// Day is a div with className
+// Week is a list of days
+// FullTrainingCal is a list of weeks...?
+
+const TrainingTable = () => {
   const { user } = useUser();
-  const [date, setDate] = useState(new Date());
   if (!user) return null;
-  return (
-    <div>
-      <Calendar
-        onChange={(event) => {
-          if (event) setDate(event as Date);
-        }}
-        value={date}
-        className={"react-calendar"}
-        showNeighboringMonth={false}
-        // showWeekNumbers={true}
-      />
-      <Calendar
-        value={add(new Date(), { weeks: 4 })}
-        className={"react-calendar"}
-        showNeighboringMonth={false}
-      />
-      <Calendar
-        value={add(new Date(), { weeks: 8 })}
-        className={"react-calendar"}
-        showNeighboringMonth={false}
-      />
-      <Calendar
-        value={add(new Date(), { weeks: 12 })}
-        className={"react-calendar"}
-        showNeighboringMonth={false}
-      />
-    </div>
-  );
 };
-
 const Home: NextPage = () => {
   const { data, isLoading } = api.posts.getAll.useQuery();
   if (isLoading) return <div>Loading...</div>;
@@ -79,7 +126,7 @@ const Home: NextPage = () => {
             Climb <span className="text-[hsl(280,100%,70%)]">HARDER</span>{" "}
           </h1>
           <div className="flex flex-col items-center gap-2">
-            <FullTrainingCalendar />
+            <CalendarWizard />
             <div>
               {data?.map((post) => (
                 <div key={post.id}>{post.content}</div>
