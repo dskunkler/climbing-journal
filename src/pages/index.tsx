dbe138eaeb978de9492a19bd555d621c01ca01fe
Daniel Kunkler <kunkl007@umn.edu>
2023-05-21 21:25:11 -0500

    Fixed onclick function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -220,7 +220,7 @@ const Home: NextPage = () => {
           <div className="flex flex-col items-center gap-2">
             {!showCalendar && (
               <button
-                onClick={setShowCalendar(true)}
+                onClick={() => setShowCalendar(true)}
                 className="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
               >
                 Get Started
