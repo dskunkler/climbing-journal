a196d2c16b0caa0ba024c7794158604c54d4ee5c
Daniel Kunkler <kunkl007@umn.edu>
2023-05-24 22:41:43 -0500

    Was getting a prerender build error, tryhing out new date

--- src/pages/components/day.tsx
+++ src/pages/components/day.tsx
@@ -5,11 +5,13 @@ export type DayProps = { date: Date; events: CycleEvent[] };
 
 export const Day = (props: DayProps) => {
   const { date, events } = props;
-  const dayNum = date.getDate();
+  const newDate = new Date(date);
+
+  const dayNum = newDate.getDate();
   return (
     <div className="relative p-2">
       <div className="absolute bottom-0 right-1">
-        {dayNum == 1 ? `${date.getUTCMonth() + 1}/${dayNum}` : `${dayNum}`}
+        {dayNum == 1 ? `${newDate.getUTCMonth() + 1}/${dayNum}` : `${dayNum}`}
       </div>
       {events.map((event, index) => (
         <ul
