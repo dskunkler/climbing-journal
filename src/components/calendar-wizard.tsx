ce1397cbd9c934dd2954a70b9bed400dfb82dbc3
Daniel Kunkler <kunkl007@umn.edu>
2023-06-07 22:43:11 -0500

    Added a color key

--- src/components/calendar-wizard.tsx
+++ src/components/calendar-wizard.tsx
@@ -1,7 +1,7 @@
 import Calendar from "react-calendar";
 import { useState, useEffect } from "react";
 import { useUser } from "@clerk/nextjs";
-import MacroCycle from "./macro-cycle";
+import MacroCycle, { MacroKey } from "./macro-cycle";
 
 export const CalendarWizard = () => {
   const { user } = useUser();
@@ -12,14 +12,17 @@ export const CalendarWizard = () => {
   if (!user) return null;
   return (
     <div>
-      <Calendar
-        onChange={(event) => {
-          if (event) setDate(event as Date);
-        }}
-        value={date}
-        calendarType="US"
-        className={"react-calendar"}
-      />
+      <div className="flex justify-center">
+        <Calendar
+          onChange={(event) => {
+            if (event) setDate(event as Date);
+          }}
+          value={date}
+          calendarType="US"
+          className={"react-calendar"}
+        />
+        {date && <MacroKey />}
+      </div>
       {date && <MacroCycle startDate={date} />}
     </div>
   );
